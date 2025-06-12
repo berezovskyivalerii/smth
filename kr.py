1fb51b2e1806e8c3032592f0a2842669724d1e7c
Valeriy <valeriiberezovskiy15@gmail.com>
2025-06-12 18:29:16 +0200

    last hm

--- kr.py
+++ kr.py
@@ -1,76 +1,81 @@
-#Напиши функцію, яка приймає рядок і повертає найдовше слово.
-def longestWord(s):
-    return max(s.split(), key=len)
+# 1) Генератор паролів
+# Завдання: Створити клас PasswordGenerator, який:
+# при виклику генерує новий випадковий пароль (__call__),
+# має @staticmethod для перевірки складності пароля,критерії:
+#         - довжина ≥ 8
+#         - містить цифру, велику та малу літери, спецсимвол
 
-print(longestWord("wwww www wwwwwww ww w"))
-
-#Модель оцінювання працівників: Клас Employee із закритими атрибутами name, ratings (список оцінок 0–5). Перевірка через дескриптор.
-class RatingDescriptor:
-    def __get__(self, instance, owner):
-        return instance.__dict__.get('_ratings', [])
-    
-    def __set__(self, instance, value):
-        if not all(isinstance(x, int) and 0 <= x <= 5 for x in value):
-            raise ValueError("не должно быть больше 5 или меньше 0")
-        instance.__dict__['_ratings'] = value
+# має @classmethod для створення генератора з заданою довжиною.
+import random
+import string
         
-class Employee:
-    __name = "Valerii"
-    ratings = RatingDescriptor()    
-    
-    def __init__(self, name: str, ratings: list):
-        self.__name = name
-        self.ratings = ratings
+class PasswordGenerator:
+    def __init__(self, length=12):
+        self.length = length
+        self.password = self.generate_password()
+
+    def __call__(self):     # при виклику генерує новий випадковий пароль (__call__),
+        return self.password
+
+    @staticmethod
+    def is_strong(password):    # має @staticmethod для перевірки складності пароля
+        if len(password) < 8:
+            return False
+        if not any(c.isdigit() for c in password):
+            return False
+        if not any(c.islower() for c in password):
+            return False
+        if not any(c.isupper() for c in password):
+            return False
+        if not any(c in "!@#$%^&[]{}-_=+*|;:(),.<>?/" for c in password):
+            return False
+        return True
+
+    @classmethod    # має @classmethod для створення генератора з заданою довжиною.
+    def with_length(cls, length: int):
+        return cls(length)
+
+    def generate_password(self):
+        characters = string.ascii_letters + string.digits + "!@#$%^&*()-_=+[]{}|;:,.<>?/"
+        password = ''.join(random.choice(characters) for _ in range(self.length))
         
-    def avg_rating(self):
-        return sum(self.ratings) / len(self.ratings)
-    
-    @property
-    def name(self):
-        return self.__name
-
-#Клас Company, який містить список працівників. Реалізувати: Метод add_employee(employee) — додає працівника,
-#якщо він ще не доданий. Метод top_performer() — повертає працівника з найвищою середньою оцінкою.
-#Метод average_company_rating() — повертає середню оцінку по компанії. Зробити Company функтором — при виклику повертати:
-#"У компанії <n> працівників, середній рейтинг: <avg>"</avg>
-
-class Company: 
-    def __call__(self):
-        return f'У компанії {len(self._employees)} працівників, середній рейтинг: {self.average_company_rating()}'
+        while not self.is_strong(password):
+            password = ''.join(random.choice(characters) for _ in range(self.length))
         
-    def __init__(self, employees):
-        self._employees = employees if employees else []
-        
-    def add_employee(self, employee):
-        if employee not in self._employees:
-            self._employees.append(employee)
-        
-    def top_performer(self):
-        return max(self._employees, key=lambda x: x.avg_rating())
+        return password
+
+generator = PasswordGenerator.with_length(8)
+print("Password:", generator())
+print(PasswordGenerator.is_strong(generator()))
+
+generator2 = PasswordGenerator()
+print("Password 2:", generator2())
+print(PasswordGenerator.is_strong(generator2()))
+
+# 2) ЗАДАЧА Менеджер знижок
+# Завдання: Створити клас DiscountManager, який:
+# викликається для розрахунку нової ціни (__call__),
+# має @staticmethod для перевірки знижки,
+# має @classmethod для створення менеджера з типовою знижкою.
+class DiscountManager:
+    def __init__(self, discount=0.1):
+        self.discount = discount
+
+    def __call__(self, price):      # викликається для розрахунку нової ціни (__call__),
+        return self.calculate_new_price(price)
+
+    @staticmethod       # має @staticmethod для перевірки знижки,
+    def is_valid_discount(discount):
+        return 0 <= discount <= 1
+
+    @classmethod        # має @classmethod для створення менеджера з типовою знижкою.
+    def with_default_discount(cls):
+        return cls(0.5)
+
+    def calculate_new_price(self, price):
+        if not self.is_valid_discount(self.discount):
+            raise ValueError("Invalid discount value")
+        return price * (1 - self.discount)
     
-    def average_company_rating(self):
-        s = sum(x.avg_rating() for x in self._employees)
-        return s / len(self._employees)
-    
-e1 = Employee("димон", [4, 5, 5])
-e2 = Employee("антон", [4,5, 4]) # OK
-#e2 = Employee("антон", [4,65, 4]) # NE OK
-e3 = Employee("амон", [2, 3])
-
-company = Company(None)
-company.add_employee(e2)
-company.add_employee(e3)
-company.add_employee(e1) 
-
-print(company()) # __call__
-print(company.top_performer().name) 
-print(company.average_company_rating())
-
-#Напиши функцію, яка приймає список чисел і повертає новий список із сумою префіксів: (тобто [a, b, c] → [a, a+b, a+b+c])
-
-def add_pref(l: list):
-    j = 0
-    for i in range(0, len(len)):
-        for k in range(0, j):
-            
-    return None
+discount_manager = DiscountManager.with_default_discount()
+print("Default discount:", discount_manager(181)) #90.5
